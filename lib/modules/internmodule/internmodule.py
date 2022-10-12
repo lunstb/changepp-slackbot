e9861ab4d3c515ea29ea16b19039a81f0a7b4636
Kylie Burgess <burgess21k@gmail.com>
2022-10-11 22:03:53 -0500

    Fixed a bunch of silly things and tested.

--- lib/modules/internmodule/internmodule.py
+++ lib/modules/internmodule/internmodule.py
@@ -14,16 +14,17 @@ env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 class InternModule(ModuleTemplate):
-
     def interpret_message(self, msg, email, db, admin) -> Dict:
         commands = msg["text"].split()
 
         if commands[0] == 'intern':
             commands = commands[1:]
             if commands[0] == 'add':
-                intern_commands = ''.join(commands[1:])
-                intern_commands = [c.strip() for c in intern_commands.split('"') if c.strip()]
-                if len(commands) == 4:
+                intern_commands = ' '.join(commands[1:]).split('"')
+                print(intern_commands)
+                intern_commands = [c.rstrip().lstrip() for c in intern_commands]
+                intern_commands = [c for c in intern_commands if c]
+                if len(intern_commands) == 3:
                     return {
                         "command": Commands.INTERN_ADD_ME,
                         "company": intern_commands[0],
@@ -53,6 +54,7 @@ class InternModule(ModuleTemplate):
     
     def process_message(self, interpretation, client, req, email, db: database, admin) -> Dict:
         if interpretation["command"] == Commands.INTERN_ADD_ME:
+            print(interpretation["company"], interpretation["position"], interpretation["accepting_refs"])
             db.insert_intern(email, interpretation["company"], interpretation["position"], interpretation["accepting_refs"])
             return added_intern()
         if interpretation["command"] == Commands.INTERN_REMOVE:
